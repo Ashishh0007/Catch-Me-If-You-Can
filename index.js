5642f9065b257f1e471fd5704c8c5bfcd020a594
Ashish Kumar Patel <105703743+Ashishh0007@users.noreply.github.com>
2023-02-10 21:17:53 +0530

    index.js updated

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//box catching functionality
 function catchMe(el) {
     el.style.top = getRandom(0, (window.innerHeight - el.offsetHeight)) + 'px';
     el.style.left = getRandom(0, (window.innerWidth - el.offsetWidth)) + 'px';
@@ -12,11 +13,26 @@ function getRandom(min, max) {
 }
 
 
+
+
+//audio section
 var audio = new Audio('katy-perry-harleys-in-hawaii-(slowed).mp3');
 document.body.onclick = () => { audio.play(); }
 audio.muted = false;
 audio.volume = 0.1;
 audio.loop = true;
+
+
+
+
+//Title functionality
+$(function() {
+    setTimeout(function() {
+        $("#music-info").fadeOut(1500);
+    }, 4000);
+
+})
+
 
 
 
